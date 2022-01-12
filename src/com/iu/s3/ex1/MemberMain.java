0a2211ebe1c37c7e73e0b2996f45f54170af8c8f
seo1163 <seo1163@naver.com>
2022-01-12 15:34:45 +0900

    mak124

--- src/com/iu/s3/ex1/MemberMain.java
+++ src/com/iu/s3/ex1/MemberMain.java
@@ -1,18 +1,20 @@
 package com.iu.s3.ex1;
 
+import java.util.ArrayList;
+
 public class MemberMain {
 
 	public static void main(String[] args) {
 		
 		MemberData memberData= new MemberData();
-		MemberDTO[] members = memberData.init();
+		ArrayList<MemberDTO> members = memberData.init();
 		
-		for(int i=0;i<members.length;i++) {
-			System.out.println(members[i].getId());
-			System.out.println(members[i].getName());
-			System.out.println(members[i].getPw());
-			System.out.println(members[i].getEmail());
-			System.out.println(members[i].getAge());
+		for(int i=0;i<members.size();i++) {
+			System.out.println(members.get(i).getId());
+			System.out.println(members.get(i).getName());
+			System.out.println(members.get(i).getPw());
+			System.out.println(members.get(i).getEmail());
+			System.out.println(members.get(i).getAge());
 			System.out.println("============================");
 		}
 		
